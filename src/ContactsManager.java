040a453f6f7c8a9516de31d79769f327cd168b77
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-02-10 12:04:09 -0600

    made contacts.txt

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -1,8 +1,37 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class ContactsManager {
 
     public static void main (String [] args) {
+
+        contactsFile();
+
+    }
+
+    static void contactsFile() {
+        String directory = "data";
+        String filename = "contacts.txt";
+
+        Path dataDirectory = Paths.get(directory);
+        System.out.println(dataDirectory.toAbsolutePath());
+        Path dataFile = Paths.get(directory,filename);
+
+        try {
+            if (Files.notExists(dataDirectory)) {
+                Files.createDirectories(dataDirectory);
+                System.out.println("Created directory");
+            }
+            if (!Files.exists(dataFile)) {
+                Files.createFile(dataFile);
+                System.out.println("Created file");
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
 
     }
 
